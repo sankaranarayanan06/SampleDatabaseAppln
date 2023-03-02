cfa333ad49b444c0165a255c69a29a14567df8f7
sankar <sankaranarayananm@sahaj.ai>
2023-03-02 14:16:29 +0530

    clean state

--- src/main/kotlin/com/example/repository/DatabaseOperation.kt
+++ src/main/kotlin/com/example/repository/DatabaseOperation.kt
@@ -6,7 +6,7 @@ class DatabaseOperation {
     private val jdbcUrl = "jdbc:postgresql://localhost:32768/sampledb"
     val connection = DriverManager.getConnection(jdbcUrl, "postgres", "root")
 
-    fun updatePrice(price:Int){
+    fun updatePrice(price: Int) {
         val resultCount = connection.createStatement()
         val result = resultCount.executeQuery("select count(*) from item")
         result.next()
@@ -14,22 +14,22 @@ class DatabaseOperation {
 
         if (result.getInt(1) > 0) {
             val query = connection.prepareStatement("update item set price=?")
-            query.setInt(1,price)
+            query.setInt(1, price)
             query.executeUpdate()
         } else {
             val query = connection.prepareStatement("insert into item(price) values(?)")
-            query.setInt(1,price)
+            query.setInt(1, price)
             query.executeUpdate()
         }
 
         val query1 = connection.prepareStatement("insert into item_price_history(price) values(?)")
-        query1.setInt(1,price)
+        query1.setInt(1, price)
         query1.executeUpdate()
     }
 
-    fun createBuyHistory(price: Int){
+    fun createBuyHistory(price: Int) {
         val query = connection.prepareStatement("insert into purchase_details(price) values(?)")
-        query.setInt(1,price)
+        query.setInt(1, price)
         query.executeUpdate()
     }
 
@@ -37,7 +37,7 @@ class DatabaseOperation {
         val query = connection.prepareStatement("select price from item")
         val result = query.executeQuery()
         var price = 0
-        while(result.next()){
+        while (result.next()) {
             price = result.getInt("price")
         }
         return price
